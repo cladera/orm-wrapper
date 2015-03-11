554cd0852818c527bc81a725405c4e06d99804e9
marc <marc.ferrer@gmail.com>
2015-03-11 16:57:49 +0100

    Set mocha tests timeout to 5s.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,8 @@ module.exports = function (grunt) {
     mochaTest: {
       test: {
         options: {
-          reporter: 'spec'
+          reporter: 'spec',
+          timeout: 5000
         },
         src: ['test/**/*.js']
       }
