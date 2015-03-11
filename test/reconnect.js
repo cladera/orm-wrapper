39262e8bf10426cf97dd5747bcdc922cd2824ab7
marc <marc.ferrer@gmail.com>
2015-03-11 17:06:11 +0100

    Fixes an error that caused the package to multiply opened connections. Apparently this was caused by an already existing handleDisconnections of mysql driver. That function does not reconnect after a long downtime, but it reconnects perfectly from disconnections caused by wait_timeout mysql config.

--- test/reconnect.js
+++ test/reconnect.js
@@ -20,10 +20,24 @@ var testOptions = {
 
 describe('DB connection loss', function(){
   var wrapper;
-  beforeEach(function(){
+  var initialConnections;
+  before(function(done){
     wrapper = require('../index');
+    wrapper.connect(testConfig, testOptions, function(err){
+      (err === null).should.equal(true);
+      wrapper.connections.db.driver.execQuery('show status like \'%Threads_connected%\';', function(err, data){
+        (err === null).should.equal(true);
+        (wrapper.connections.db).should.be.type('object');
+        initialConnections = data[0].Value;
+        wrapper.handleDisconnections(testConfig, {retries: 10, delay: 500});
+        done();
+      });
+    });
   });
-  afterEach(function(done){
+  /*beforeEach(function(){
+    wrapper = require('../index');
+  });*/
+  after(function(done){
     wrapper.close(function(err){
       if(err){
         throw new Error(err);
@@ -32,15 +46,18 @@ describe('DB connection loss', function(){
     });
   });
   it('Should reconnect to the server after a PROTOCOL_CONNECTION_LOST event', function(done){
-    wrapper.connect(testConfig, testOptions, function(err){
+    /*wrapper.connect(testConfig, testOptions, function(err){
       (err === null).should.equal(true);
       (wrapper.connections.db).should.be.type('object');
       wrapper.connections.db.should.not.have.property('connectionRetries');
       wrapper.handleDisconnections(testConfig, {retries: 10, delay: 500});
       wrapper.connections.db.connectionRetries.should.equal(0);
       wrapper.connections.db.should.have.property('emit');
-      wrapper.connections.db.emit('error', 'PROTOCOL_CONNECTION_LOST');
+      var error = {code: 'PROTOCOL_CONNECTION_LOST'};
+      error.fatal = true;
+      wrapper.connections.db.emit('error', error);
       wrapper.connections.db.connectionRetries.should.equal(1);
+      //wrapper.connections.db.driver.execQuery('SET SESSION wait_timeout = 2;');
       var timer = setTimeout(function(){
         wrapper.connections.db.connectionRetries.should.equal(0);
         wrapper.connections.db.driver.execQuery('show tables', function(err){
@@ -51,6 +68,53 @@ describe('DB connection loss', function(){
           done();
         });
       }, 1000);
+    });*/
+    wrapper.connections.db.connectionRetries.should.equal(0);
+    wrapper.connections.db.should.have.property('emit');
+    var error = {code: 'PROTOCOL_CONNECTION_LOST'};
+    error.fatal = true;
+    wrapper.connections.db.emit('error', error);
+    wrapper.connections.db.connectionRetries.should.equal(1);
+    //wrapper.connections.db.driver.execQuery('SET SESSION wait_timeout = 2;');
+    var timer = setTimeout(function(){
+      wrapper.connections.db.connectionRetries.should.equal(0);
+      wrapper.connections.db.driver.execQuery('show tables', function(err){
+        clearTimeout(timer);
+        if(err){
+          throw new Error(err);
+        }
+        done();
+      });
+    }, 1000);
+  });
+  describe('Showing DB connections', function(){
+    before(function(done){
+      setTimeout(function(){
+        done();
+      }, 2500);
+    });
+    it('Should reconnec using new disconnections handler', function(done){
+      wrapper.connections.db.driver.opts.settings.set('connection.reconnect', false);
+      wrapper.connections.db.driver.execQuery('SET SESSION wait_timeout = 2;', function(err){
+        (err === null).should.equal(true);
+        //We expect the connectin to be closed after setting wait_timeout to 2s
+        var timer2 = setTimeout(function(){
+          clearTimeout(timer2);
+          wrapper.connections.db.driver.execQuery('show tables', function(err){
+            (err === null).should.equal(false);
+          });
+        }, 2100);
+        var timer = setTimeout(function(){
+          clearTimeout(timer);
+          wrapper.connections.db.connectionRetries.should.equal(0);
+          wrapper.connections.db.driver.execQuery('show status like \'%Threads_connected%\';', function(err, data){
+            (err === null).should.equal(true);
+            //Check that new disconnections handler do not create extra connections
+            data[0].Value.should.equal(initialConnections);
+            done();
+          });
+        }, 2700);
+      });
     });
   });
 });
